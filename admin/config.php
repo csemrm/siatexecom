ad9e018c8b623eccbb793a0014ed92cf59fe9660
Md. Mostafizur Rahman <csemrm@gmail.com>
2014-05-21 13:35:02 +0600

    layout added product

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8085/admin/');
+define('HTTP_CATALOG', 'http://localhost:8085/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8085/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8085/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\project\siatexecom/admin/');
+define('DIR_SYSTEM', 'F:\project\siatexecom/system/');
+define('DIR_DATABASE', 'F:\project\siatexecom/system/database/');
+define('DIR_LANGUAGE', 'F:\project\siatexecom/admin/language/');
+define('DIR_TEMPLATE', 'F:\project\siatexecom/admin/view/template/');
+define('DIR_CONFIG', 'F:\project\siatexecom/system/config/');
+define('DIR_IMAGE', 'F:\project\siatexecom/image/');
+define('DIR_CACHE', 'F:\project\siatexecom/system/cache/');
+define('DIR_DOWNLOAD', 'F:\project\siatexecom/download/');
+define('DIR_LOGS', 'F:\project\siatexecom/system/logs/');
+define('DIR_CATALOG', 'F:\project\siatexecom/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'siatexe');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8085/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8085/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\project\siatexecom/catalog/');
+define('DIR_SYSTEM', 'F:\project\siatexecom/system/');
+define('DIR_DATABASE', 'F:\project\siatexecom/system/database/');
+define('DIR_LANGUAGE', 'F:\project\siatexecom/catalog/language/');
+define('DIR_TEMPLATE', 'F:\project\siatexecom/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\project\siatexecom/system/config/');
+define('DIR_IMAGE', 'F:\project\siatexecom/image/');
+define('DIR_CACHE', 'F:\project\siatexecom/system/cache/');
+define('DIR_DOWNLOAD', 'F:\project\siatexecom/download/');
+define('DIR_LOGS', 'F:\project\siatexecom/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'siatexe');
+define('DB_PREFIX', 'oc_');
+?>
