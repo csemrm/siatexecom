9c5bd8106e59548b028d7aaaebd0b3416f58b3c7
Md. Mostafizur Rahman <csemrm@gmail.com>
2014-06-19 17:27:20 +0600

    seo url updated and theme name updated

--- index.php
+++ index.php
@@ -13,18 +13,21 @@ if (!defined('DIR_APPLICATION')) {
 	exit;
 }
 
+// VirtualQMOD
+require_once('./vqmod/vqmod.php');
+VQMod::bootup();
 
-// Startup
-require_once(DIR_SYSTEM . 'startup.php');
+// VQMODDED Startup
+require_once(VQMod::modCheck(DIR_SYSTEM . 'startup.php'));
 
 // Application Classes
-require_once(DIR_SYSTEM . 'library/customer.php');
-require_once(DIR_SYSTEM . 'library/affiliate.php');
-require_once(DIR_SYSTEM . 'library/currency.php');
-require_once(DIR_SYSTEM . 'library/tax.php');
-require_once(DIR_SYSTEM . 'library/weight.php');
-require_once(DIR_SYSTEM . 'library/length.php');
-require_once(DIR_SYSTEM . 'library/cart.php');
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/customer.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/affiliate.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/currency.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/tax.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/weight.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/length.php'));
+require_once(VQMod::modCheck(DIR_SYSTEM . 'library/cart.php'));
 
 // Registry
 $registry = new Registry();
